26dd8a742b6e0bc2561dc31278609aa9b424aeb7
u20463104 <134299073+u20463104@users.noreply.github.com>
2024-10-22 23:39:50 +0200

    files

---
+++ CityGrowth.h
@@ -0,0 +1,24 @@
+#ifndef CITYGROWTH_H
+#define CITYGROWTH_H
+
+class CityGrowthObserver{
+public:
+    virtual void update(int population) = 0;
+};
+
+class Housing : public CityGrowthObserver {
+public:
+    void update(int population) override;
+};
+
+class Economy : public CityGrowthObserver {
+public:
+    void update(int population) override;
+};
+
+class Infrastructure : public CityGrowthObserver {
+public:
+    void update(int population) override;
+};
+
+#endif
---
+++ Strategy.cpp
@@ -0,0 +1,11 @@
+#include "Strategy.h"
+
+std::string PopulationCategorization::categorize(int population, int economy)
+{
+    return std::string();
+}
+
+std::string EconomyCategorization::categorize(int population, int economy)
+{
+    return std::string();
+}
