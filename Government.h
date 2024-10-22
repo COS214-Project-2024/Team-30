5c030317837ec892cb1e501ac6f10eadfcd7247e
u20463104 <134299073+u20463104@users.noreply.github.com>
2024-10-22 23:44:11 +0200

    files addition

---
+++ Government.h
@@ -0,0 +1,8 @@
+#ifndef GOVERNMENT_H
+#define GOVERNMENT_H
+
+class Government{
+
+};
+
+#endif
---
+++ Population.h
@@ -0,0 +1,12 @@
+#ifndef POPULATION_H
+#define POPULATION_H
+
+class Population {
+    public:
+        int getPopulation();
+        void setPopulation();
+    private: 
+     int population;
+};
+
+#endif
