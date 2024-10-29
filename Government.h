0b53a1ffc396c4c2b011a6fa7633393d66b3a3a3
u22668323 <u22668323@tuks.co.za>
2024-10-29 20:58:03 +0200

    added to government

--- Government.h
+++ Government.h
@@ -2,17 +2,44 @@
 #define GOVERNMENT_H
 
 #include <vector>
-#include "CityGrowth.h"
-#include "Strategy.h"
+#include <iostream>
+#include <string>
+#include <memory>
+#include <random>
+#include "CityGrowthObserver.h"
+#include "CategorizationStrategy.h"
+#include "Citizen.h"
+#include "Building.h"
+#include "BuildingFactory.h"
+using namespace std;
 class Government{
+private:
+//lists
+    std::vector<CityGrowthObserver*> observers;
+    std::vector<Citizen*> population;
+    std::vector<Building*> Infurstructure;
+//objects
+    BuildingFactory* CityBuilder;
+    CategorizationStrategy* strategy;
+//data    
+    string cityName;
+    double employmentRate;
+    int citybudget;
+
+
 public:
+    Government(string);
     void attach(CityGrowthObserver ob);
     void detach(CityGrowthObserver ob);
     void notifyObservers();
+    void populationGrowth(Citizen*);
+    void calculateEmploymentRate();
+    void increaseInfurstructure();
+    
 
-private:
-    std::vector<CityGrowthObserver*> observers;
-    CategorizationStrategy* strategy;
+
+
+
 };
 
 #endif
