49095a32dedc2dda71b90a8783a1d69b3c7bea53
Lonwabo Kwitshana <lonwabo.kwitshana@gmail.com>
2024-11-02 22:04:09 +0200

    starting with gov unit tests

---
+++ UnitTests/Government/GovernmentUnitTests.cpp
@@ -0,0 +1,13 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../doctest.h"
+#include "../../src/Citizen.h"
+#include "../../src/Building.h"
+#include "../../src/Residential.h"
+#include "../../src/Government.h"
+#include "../../src/Fires.h"
+#include "../../src/Earthquake.h"
+#include "../../src/Building.h"
+#include "../../src/EmergencyServices.h"
+#include "../../src/Emergencies.h"
+#include "../../src/Thieves.h"
+
