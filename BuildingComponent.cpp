06de9d8ec52d4b8a734de4e271615404c426e9ec
alishaperumal <alishaperumal11@gmail.com>
2024-11-03 04:36:16 +0200

    added Building Component cpp file

---
+++ BuildingComponent.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include "BuildingComponent.h"
+
+void BuildingComponent::add(std::unique_ptr<BuildingComponent> component){
+
+}
+
+void BuildingComponent::remove(const BuildingComponent *component){
+    
+}
