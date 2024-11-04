aed27951c6ad1000f19916c02a5439d8901a8f39
diyaxbudhia <40611237+diyaxbudhia@users.noreply.github.com>
2024-11-05 01:26:50 +0200

    factory method (resources)

--- ResourceFactory.h
+++ ResourceFactory.h
@@ -1,16 +1,15 @@
 #ifndef RESOURCE_FACTORY_H
 #define RESOURCE_FACTORY_H
 #include "Resource.h"
+using namespace std;
+#include <string>
+#include <memory>
 
 class ResourceFactory
 {
     public: 
         ResourceFactory();
-        virtual Resource* createWood() =0;
-        virtual Resource* createSteel() =0;
-        virtual Resource* createWater() =0;
-        virtual Resource* createConcrete() =0;
-        virtual Resource* createBudget() =0; 
-
+        virtual ~ResourceFactory() = default; 
+        virtual std::unique_ptr<Resource> createResource(const std::string& type, int capacity) = 0;
 };
 #endif
