b9c882276c001ae4312625f6d6a474d74d582612
charlotte-lauren <charlotte.green@tuks.co.za>
2024-11-04 09:16:27 +0200

    Utilities.cpp Notify() updated

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,13 +1,15 @@
 #include "Utilities.h"
+#include "Citizen.h"
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-Utilities::Utilities(Building b){
+Utilities::Utilities(Building b)
+{
     cout << "Providing city utilities to building...\n";
-    
+
     powerPlant.generateElectricity();
 
     waterSupply.distributeWater();
@@ -19,6 +21,21 @@ Utilities::Utilities(Building b){
     sewageSystem.manageTreatment(Building b);
 }
 
-void Utilities::notifyCitizens(const string& message) {
-    cout << "Notification to citizens: " << message << endl;
+void Utilities::notifyCitizens(const string &message)
+{
+    // for (Citizen* citizen : citizens) {
+    //     if (working) {
+    //         citizen->increaseSatisfaction(10);  // increase satisfaction if working
+    //     } else {
+    //         citizen->decreaseSatisfaction(10);  // decrease satisfaction if not working
+    //     }
+    // }
+
+    cout << message << endl;;
+
+    vector<Citizen *>::iterator it = residents.begin();
+    for (it = residents.begin(); it != residents.end(); ++it)
+    {
+        (*it)->update();
+    }
 }
