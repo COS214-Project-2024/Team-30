338cec9d25480c773617e5f5b1ab572d34de1a2f
alishaperumal <141569537+alishaperumal@users.noreply.github.com>
2024-11-05 01:06:31 +0200

    Update CommercialFactory.h

--- CommercialFactory.h
+++ CommercialFactory.h
@@ -5,6 +5,13 @@
 #include "BuildingFactory.h"
 #include <memory> // Include for smart pointers
 
+/**
+ * @class CommercialFactory
+ * @brief Factory class for creating Commercial Building objects.
+ *
+ * The CommercialFactory class implements the BuildingFactory interface to create instances of commercial buildings.
+ */
+
 class CommercialFactory : public BuildingFactory {
     public:
     unique_ptr<Building> createBuilding() override; // Change return type to unique_ptr<Building>
