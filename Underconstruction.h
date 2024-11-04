c45777d825c186558172c9449d96ef56351c2a4e
alishaperumal <141569537+alishaperumal@users.noreply.github.com>
2024-11-05 01:25:01 +0200

    Update Underconstruction.h

--- Underconstruction.h
+++ Underconstruction.h
@@ -3,12 +3,26 @@
 
 #include "BuildingState.h"
 
+/**
+ * @class Underconstruction
+ * @brief Represents the "Under Construction" state of a Building.
+ *
+ * The Underconstruction class implements state-specific behavior for buildings
+ * that are currently under construction. It provides concrete implementations
+ * of the methods defined in the BuildingState interface.
+ */
+
 class Underconstruction : public BuildingState{
 
     private:
     protected:
 
     public:
+    /**
+     * @brief Handles the behavior specific to the "Under Construction" state.
+     *
+     * This method defines the actions taken while the building is in the "Under Construction" state.
+     */
     void handle() override;
     void changeState() override;
     string getStatus() override;
