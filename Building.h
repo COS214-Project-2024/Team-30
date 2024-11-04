41cfb9419524279ad6d3eeebe40c8286e2117d01
Lonwabo Kwitshana <162803818+7onwabo@users.noreply.github.com>
2024-11-04 21:43:03 +0200

    Update Building.h

--- Building.h
+++ Building.h
@@ -2,29 +2,31 @@
 #define BUILDING_H
 
 #include <vector>
+#include <memory> // Include for smart pointers
 #include <algorithm>
-#include <memory>  // Include for smart pointers
-
 #include "BuildingState.h"
-#include "Utilities.h"
+// #include "Utilities.h"
 #include "BuildingComponent.h"
-#include "Citizen.h"
 #include "Emergencies.h"
-
-using namespace std;
+#include "Citizen.h"
 
 class Emergencies;
 
-class Building : public BuildingComponent{
-    protected:
-    std::string name;
-    int capacity; 
+using namespace std;
+//
+class Building
+//  : public BuildingComponent
+{
+protected:
+    // vector<Utilities*> utilities;
+    int id;
+    unique_ptr<BuildingState> currState; // Use unique_ptr for ownership
+    int capacity;
     int buildingHealth;
-    int price; //price is gonna work with city budget in government
+    int price; //? whats price : price is gonna work with city budget in government
     bool runningUtils;
-    std::unique_ptr<BuildingState> currState; // Use unique_ptr for ownership
-
     vector<shared_ptr<Citizen>> residents;
+    static int nextID;
 
     //Utilities
     int water;
@@ -32,23 +34,38 @@ class Building : public BuildingComponent{
     int waste;
     int sewerage;
 
-    public:
-    //Building();
-    //Building(const std::string& name);        //composite constructor
+
+public:
+    Building(); //Building constructor
+    // Building(BuildingState* initialState); //set state to underconstruction
     virtual ~Building();
     int getCapacity();
-    virtual std::string getType() = 0;
     virtual void build() = 0;
-    BuildingState* getState();
-    void setState(unique_ptr<BuildingState> state); // Accept a unique_ptr
-    void processState();    
-    void displayInfo();
-    void update();  
-    virtual unique_ptr<Building> clone() = 0; 
-    void showInfo() const override; //composite
+    BuildingState *getState();
+    void setState(unique_ptr<BuildingState> state); // Accept a unique_ptr
+    void setCapacity(int capacity);
+    void processState(); // request() function for State DP
+    virtual string getType() = 0;
+    void displayInfo(); // removed override keyword. testing without the building component
+    void simulateEmergency(Emergencies &emergency);
+    bool hasOccupant(int citizenID) const;
+    void printResidents();
+    bool containsCitizen(shared_ptr<Citizen> citizen);
+    string getName();
+    int getID();
+    // void add(std::unique_ptr<BuildingComponent> component) override;
+    // void remove(std::unique_ptr<BuildingComponent> component) override;
+    void takeDamage(int damage);
+    void update();
+    void recieveUtilities(); // Utilities
+    // Utilities* setUtilities();
+
+    void addCitizen(shared_ptr<Citizen> citizen);
+    void removeCitizen(shared_ptr<Citizen> citizen);
+    void notifyCitizensOfEmergency(int damage);
+
 
     void setUtilities();
-    void requestUtilities();
     
     void setWater(int water);
     void setPower(int power);
@@ -60,20 +77,7 @@ class Building : public BuildingComponent{
     int getSewerage();
     int getWaste();
 
-    void addCitizen(shared_ptr<Citizen> citizen);
-    void removeCitizen(shared_ptr<Citizen> citizen);
-    void notifyCitizensOfEmergency(int damage);
-
-    void simulateEmergency(Emergencies &emergency);
-    bool hasOccupant(int citizenID) const;
-
-    void simulateEmergency(Emergencies &emergency);
-    bool hasOccupant(int citizenID) const;
-
-    void printResidents();
-    string getName();
-    bool containsCitizen(shared_ptr<Citizen> citizen);
-
+    virtual unique_ptr<Building> clone() = 0; // Return unique_ptr
 };
 
 #endif
