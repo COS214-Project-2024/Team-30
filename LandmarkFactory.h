c31358c0302e4148677f0c25e0b9bc41ae951de8
alishaperumal <141569537+alishaperumal@users.noreply.github.com>
2024-11-05 01:13:49 +0200

    Update LandmarkFactory.h

--- LandmarkFactory.h
+++ LandmarkFactory.h
@@ -5,6 +5,13 @@
 #include "BuildingFactory.h"
 #include <memory> // Include for smart pointers
 
+/**
+ * @class LandmarkFactory
+ * @brief Factory class for creating Landmark Building objects.
+ *
+ * The LandmarkFactory class implements the BuildingFactory interface to create instances of landmark buildings.
+ */
+
 class LandmarkFactory : public BuildingFactory {
 
     private:
