a0bf3f1d9038372109c50b711798292e347fe8aa
alishaperumal <141569537+alishaperumal@users.noreply.github.com>
2024-11-05 01:12:15 +0200

    Update IndustrialFactory.h

--- IndustrialFactory.h
+++ IndustrialFactory.h
@@ -17,6 +17,12 @@ class IndustrialFactory : public BuildingFactory {
     private:
     protected:
     public:
+    /**
+     * @brief Creates a new Industrial Building object.
+     *
+     * Overrides the createBuilding method to create an instance of an industrial building.
+     * @return A unique pointer to a newly created industrial Building object.
+     */
     unique_ptr<Building> createBuilding() override; // Change to return unique_ptr<Building>
 };
 
