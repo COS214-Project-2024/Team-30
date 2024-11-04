865ef16949d328131f9ff351479dfb451eb3ca65
alishaperumal <141569537+alishaperumal@users.noreply.github.com>
2024-11-05 01:33:16 +0200

    Update Built.h

--- Built.h
+++ Built.h
@@ -3,6 +3,15 @@
 
 #include "BuildingState.h"
 
+/**
+ * @class Built
+ * @brief Represents the "Built" state of a Building.
+ *
+ * The Built class implements state-specific behavior for buildings that are fully constructed
+ * and operational. It provides concrete implementations of the methods defined in the BuildingState
+ * interface for managing this state.
+ */
+
 class Built : public BuildingState{
 
     private:
