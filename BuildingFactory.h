2071c9013e564e24ddebf5565ad781e731160584
alishaperumal <141569537+alishaperumal@users.noreply.github.com>
2024-11-05 01:01:48 +0200

    Update BuildingFactory.h

--- BuildingFactory.h
+++ BuildingFactory.h
@@ -3,6 +3,14 @@
 
 #include "Building.h"
 
+/**
+ * @class BuildingFactory
+ * @brief Abstract factory class for creating Building objects.
+ *
+ * The BuildingFactory class provides an interface for creating different types of buildings.
+ * Derived factory classes must implement the createBuilding method to instantiate specific building types.
+ */
+
 class BuildingFactory
 {
 public:
