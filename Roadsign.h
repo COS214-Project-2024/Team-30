2521480b44fbae0118203349e8863bd08c742ab8
JoshKretschmer <151640473+JoshKretschmer@users.noreply.github.com>
2024-11-05 06:32:25 +0200

    Update Roadsign.h

--- Roadsign.h
+++ Roadsign.h
@@ -8,18 +8,17 @@ using namespace std;
 /**
  * @brief Represents a road sign in the traffic system.
  *
- * The RoadSign class inherits from the Sign class and represents a sign specifically
- * designed for roads, providing information or instructions to drivers.
+ * Implaments the template design pattern, and gives road sign functionality to drivers
  */
 class RoadSign : public Sign {
 public:
     /**
      * @brief Constructs a RoadSign object with a specified message.
      *
-     * @param message The message displayed on the road sign.
+     * @param message The message displayed 
      */
     RoadSign(const string& message) : Sign(message) {
-        type = "RoadSign"; ///< Sets the type of sign to "RoadSign".
+        type = "RoadSign"; ///< Sets type
     }
 };
 
