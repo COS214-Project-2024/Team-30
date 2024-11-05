bfb1379ff631df7e473d5d833c3aa6954e1f121e
JoshKretschmer <151640473+JoshKretschmer@users.noreply.github.com>
2024-11-05 06:28:34 +0200

    Update Taxi.h

--- Taxi.h
+++ Taxi.h
@@ -4,18 +4,17 @@
 #include "PublicTransport.h"
 
 /**
- * @brief Represents a taxi that can transport citizens between buildings.
+ * @brief Taxi 
  *
- * The Taxi class inherits from the Transport class and provides functionality
- * for traveling to a specified building while managing a list of passengers.
+ * Taxi class is the public transport that makes use of roads
  */
 class Taxi : public publicTransport {
 public:
 
     /**
-     * @brief Constructs a Taxi object with a specified name and capacity.
+     * @brief Constructs a Taxi object with a specified name and capacity
      *
-     * @param capacity The maximum number of passengers the taxi can carry.
+     * @param capacity The maximum number of passengers the taxi can carry
      * @param from The building 
      */
     Taxi(int capacity, shared_ptr<Building> from);
@@ -23,10 +22,7 @@ public:
     /**
      * @brief Travels to a specified building and transports passengers.
      *
-     * This method overrides the Travel function from the Transport class.
-     * It moves all passengers from the taxi to the specified building,
-     * updating the taxi's current location and clearing the list of
-     * passengers after transporting them.
+     * moves through the linked list to arrive at destination
      *
      * @param to A shared pointer to the destination building where passengers
      *           will be transported.
